c9fd41519c6fedfd5345a603b30edf1e4c15224d
Antonio Vuono <antoniosvuono@icloud.com>
2025-10-09 15:09:17 -0300

    feature: split document implemented

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,7 @@
 export const config = {
   knowledgeBasePath: 'data/doc-sdk.pdf',
+  textSplitter: {
+    chunkSize: 500,
+    chunkOverlap: 50,
+  },
 };
--- src/services/RagService.ts
+++ src/services/RagService.ts
@@ -1,7 +1,12 @@
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 
 interface RagConfig {
   knowledgeBasePath: string;
+  textSplitter: {
+    chunkSize: number;
+    chunkOverlap: number;
+  };
 }
 
 export class RagService {
@@ -14,9 +19,15 @@ export class RagService {
   public async initialize() {
     console.log('Inicializando o Service ....');
 
+    // 1. Load document (pdf)
     const loader = new PDFLoader(this.config.knowledgeBasePath);
     const docs = await loader.load();
 
-    console.log('Documento carregado', docs);
+    // 2. Split document
+    const splitter = new RecursiveCharacterTextSplitter(this.config.textSplitter);
+
+    const splitDocs = await splitter.splitDocuments(docs);
+
+    console.log('documento dividido!', splitDocs);
   }
 }
